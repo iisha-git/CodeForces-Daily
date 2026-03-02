bd3b7530b95110257b792310e75993a084f11c8e
Isha Singh <study.luto@gmail.com>
2026-03-02 22:05:50 +0530

    day 41

---
+++ 800/TwentyFive.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class TwentyFive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        if(n==1){
+            System.out.println("1");
+        }else{
+            System.out.println("25");
+        }
+    }
+}
