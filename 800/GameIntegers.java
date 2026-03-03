79bcdd69dcd9705986fd1cd3ce377f74677b3441
Isha Singh <study.luto@gmail.com>
2026-03-03 21:53:58 +0530

    day 42 cf

--- 800/GameIntegers.java
+++ 800/GameIntegers.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class GameIntegers {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int integer = sc.nextInt();
+            //int Fin = 0;
+            if(integer%3==1 || integer%3==2){
+                System.out.println("First");
+            }else{
+                System.out.println("Second");
+            }
+        }
+    }
 }
