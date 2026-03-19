ab04cedddf3c41456f5ae1289841dcca29d6ed22
Isha Singh <study.luto@gmail.com>
2026-03-19 23:26:58 +0530

    Box Tower

---
+++ 800/BoxTower.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class BoxTower {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int d = sc.nextInt();
+            
+            int maxHeight = (d / m) + 1;
+            
+            int towers = (n + maxHeight - 1) / maxHeight; // ceil division
+            
+            System.out.println(towers);
+        }
+    }
+}
