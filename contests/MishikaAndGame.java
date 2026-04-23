d7325ebf3923f12b06fd0e1b17db8b2464cc22e1
Isha Singh <study.luto@gmail.com>
2026-04-23 21:37:04 +0530

    Loop through all rounds, compare each pair of dice rolls, increment Mishka’s or Chris’s win counter accordingly, and after processing all rounds, compare the two counters to print the final result (or draw if equal)

---
+++ contests/MishikaAndGame.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class MishkaAndGame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int mishka = 0;
+        int chris = 0;
+
+        for (int i = 0; i < n; i++) {
+            int m = sc.nextInt();
+            int c = sc.nextInt();
+
+            if (m > c) {
+                mishka++;
+            } else if (m < c) {
+                chris++;
+            }
+        }
+
+        if (mishka > chris) {
+            System.out.println("Mishka");
+        } else if (chris > mishka) {
+            System.out.println("Chris");
+        } else {
+            System.out.println("Friendship is magic!^^");
+        }
+
+    }
+}
