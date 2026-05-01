2ccd2270a7e1aec8e26db4b7803757bf2ba328e6
Isha Singh <study.luto@gmail.com>
2026-05-01 23:52:47 +0530

    maximum profit O(n)

---
+++ leetcode/buySell.java
@@ -0,0 +1,18 @@
+public class buySell {
+    public int maxProfit(int[] prices) {
+     int min = Integer.MAX_VALUE;
+     int maxProfit = 0;
+     int profit = 0;
+     for(int i =0;i<prices.length;i++){
+        if(prices[i]<min){
+            min = prices[i];
+        }else{
+            profit = prices[i]-min;
+            if(profit>maxProfit){
+                maxProfit=profit;
+            }
+        }
+     }
+     return maxProfit;
+    }
+}
