fa84e18adb7b93f8cfeae37e0e8ac426c432ad1a
Isha Singh <study.luto@gmail.com>
2026-05-10 22:55:51 +0530

    recursive logic for reverse string

---
+++ recursion/reverseStringRecursion.java
@@ -0,0 +1,17 @@
+public class reverseStringRecursion {
+    public void reverseStringHelper(char []s,int start, int end){
+        if(start>=end){
+            return;
+        }
+        char temp = s[start];
+        s[start] = s[end];
+        s[end]= temp;
+
+        reverseStringHelper(s,start+1,end-1);
+
+    }
+    public void reverseString(char [] s){
+        reverseStringHelper(s, 0, s.length-1);
+    }
+    
+}
