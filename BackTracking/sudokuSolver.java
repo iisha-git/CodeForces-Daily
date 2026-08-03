b9a65f30ce26027829f5146c6f0927e2103b2d39
Isha Singh <study.luto@gmail.com>
2026-08-04 00:13:23 +0530

    sudoku solver

---
+++ BackTracking/sudokuSolver.java
@@ -0,0 +1,52 @@
+package BackTracking;
+
+public class sudokuSolver {
+    public void solveSudoku(char[] [] board){
+        solve(board);
+
+    }
+
+    private boolean solve(char[][] board){
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board.length; col++) {
+                if(board[row][col] == '.'){
+                    for (char num = '1'; num <= '9'; num++) {
+                       if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if(solve(board)){
+                                return true;
+                            }
+                            board[row][col] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(char[][] board,int row,int col,char num){
+        for (int i = 0; i < board.length; i++) {
+            if(board[row][i]== num){
+                return false;
+            }
+        }
+        for (int i = 0; i < board.length; i++) {
+            if(board[i][col]==num){
+                return false;
+            }
+        }
+
+        int startRow=(row/3)*3;
+        int startCol=(col/3)*3;
+        for (int i = startRow; i < startRow+3; i++) {
+            for (int j = startCol; j < startCol+3; j++) {
+                if(board[i][j]==num){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
